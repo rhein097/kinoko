928f3cc64e4e2c9a2969d4f05e7127f00196a837
rhein097 <zalzzukker@gmail.com>
2021-07-19 00:18:53 +0900

    first commit

---
+++ classify.py
@@ -0,0 +1,27 @@
+import sys
+from PIL import Image
+from keras.models import load_model
+import numpy as np
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+def main():
+    name = sys.argv[1]
+    #print(name)
+    image = Image.open(name)
+    image = image.resize((64, 64))
+    image.show()
+    model = load_model("model.h5")
+    np_image = np.array(image)
+    np_image = np_image / 255
+    np_image = np_image[np.newaxis, :, :, :]
+    result = model.predict(np_image)
+    #print(result)
+    if result[0][0] > result[0][1]:
+        print("椎茸")
+    else:
+        print("月夜茸")
+
+
+if __name__ == "__main__":
+    main()
